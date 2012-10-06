1be5afe1e117b5f7a4a7fbe933f0596d27ca9c51
cysun <cysun@cddbb36c-00e0-4825-8dd4-826f7bd89ddf>
2012-10-06 22:01:55 +0000

    Added model classes, DAO, SQL statements, and some test cases for project listing.

---
+++ trunk/src/main/java/csns/model/academics/Project.java
@@ -0,0 +1,187 @@
+/*
+ * This file is part of the CSNetwork Services (CSNS) project.
+ * 
+ * Copyright 2012, Chengyu Sun (csun@calstatela.edu).
+ * 
+ * CSNS is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ * 
+ * CSNS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
+ * more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with CSNS. If not, see http://www.gnu.org/licenses/agpl.html.
+ */
+package csns.model.academics;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OrderBy;
+import javax.persistence.OrderColumn;
+import javax.persistence.Table;
+
+import csns.model.core.Resource;
+import csns.model.core.User;
+
+@Entity
+@Table(name = "projects")
+public class Project implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @ManyToMany
+    @JoinTable(name = "project_advisors",
+        joinColumns = @JoinColumn(name = "project_id"),
+        inverseJoinColumns = @JoinColumn(name = "advisor_id"))
+    @OrderColumn(name = "advisor_order")
+    private List<User> advisors;
+
+    @ManyToMany
+    @JoinTable(name = "project_members",
+        joinColumns = @JoinColumn(name = "project_id"),
+        inverseJoinColumns = @JoinColumn(name = "member_id"))
+    @OrderBy("firstName asc")
+    private List<User> members;
+
+    @ManyToMany(cascade = { CascadeType.MERGE, CascadeType.PERSIST })
+    @JoinTable(name = "project_resources",
+        joinColumns = @JoinColumn(name = "project_id"),
+        inverseJoinColumns = @JoinColumn(name = "resource_id"))
+    @OrderColumn(name = "resource_order")
+    private List<Resource> resources;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    private int year;
+
+    @Column(nullable = false)
+    private boolean published;
+
+    public Project()
+    {
+        advisors = new ArrayList<User>();
+        members = new ArrayList<User>();
+        resources = new ArrayList<Resource>();
+
+        year = Calendar.getInstance().get( Calendar.YEAR );
+        published = false;
+    }
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId( Long id )
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName( String name )
+    {
+        this.name = name;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription( String description )
+    {
+        this.description = description;
+    }
+
+    public List<User> getAdvisors()
+    {
+        return advisors;
+    }
+
+    public void setAdvisors( List<User> advisors )
+    {
+        this.advisors = advisors;
+    }
+
+    public List<User> getMembers()
+    {
+        return members;
+    }
+
+    public void setMembers( List<User> members )
+    {
+        this.members = members;
+    }
+
+    public List<Resource> getResources()
+    {
+        return resources;
+    }
+
+    public void setResources( List<Resource> resources )
+    {
+        this.resources = resources;
+    }
+
+    public Department getDepartment()
+    {
+        return department;
+    }
+
+    public void setDepartment( Department department )
+    {
+        this.department = department;
+    }
+
+    public int getYear()
+    {
+        return year;
+    }
+
+    public void setYear( int year )
+    {
+        this.year = year;
+    }
+
+    public boolean isPublished()
+    {
+        return published;
+    }
+
+    public void setPublished( boolean published )
+    {
+        this.published = published;
+    }
+
+}

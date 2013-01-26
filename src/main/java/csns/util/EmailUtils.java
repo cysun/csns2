7461f7aeedce5ce4113c86bf43e35bafd4125d3c
Chengyu Sun <cysun@users.noreply.github.com>
2013-01-26 01:08:57 +0000

    . Added the function to email advisement records. . Added the ability to send HTML email. . Changed a bit how EmailController and EmailUtils work.

--- src/main/java/csns/util/EmailUtils.java
+++ src/main/java/csns/util/EmailUtils.java
@@ -21,11 +21,20 @@ package csns.util;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+import csns.helper.Email;
 import csns.model.core.AbstractMessage;
 import csns.model.core.File;
 import csns.model.core.Subscribable;
@@ -36,14 +45,19 @@ import csns.model.core.dao.SubscriptionDao;
 @Component
 public class EmailUtils {
 
+    @Autowired
+    SubscriptionDao subscriptionDao;
+
+    @Autowired
+    JavaMailSender mailSender;
+
     @Value("#{applicationProperties.url}")
     private String appUrl;
 
     @Value("#{applicationProperties.email}")
     private String appEmail;
 
-    @Autowired
-    SubscriptionDao subscriptionDao;
+    private static final Logger logger = LoggerFactory.getLogger( EmailUtils.class );
 
     public List<String> getAddresses( List<User> users,
         boolean useSecondaryEmail )
@@ -95,6 +109,90 @@ public class EmailUtils {
         }
 
         return sb.toString();
+    }
+
+    public String getHtml( AbstractMessage message )
+    {
+        StringBuffer sb = new StringBuffer( message.getContent() );
+
+        List<File> attachments = message.getAttachments();
+        if( attachments.size() > 0 )
+        {
+            sb.append( "<p>[Attachments]</p>" ).append( "<ul>\n" );
+            for( int i = 0; i < attachments.size(); ++i )
+                sb.append( "<li><a href='" )
+                    .append( appUrl )
+                    .append( "/download?fileId=" )
+                    .append( attachments.get( i ).getId() )
+                    .append( "'>" )
+                    .append( attachments.get( i ).getName() )
+                    .append( "</a></li>\n" );
+            sb.append( "</ul>" );
+        }
+
+        return sb.toString();
+    }
+
+    public boolean sendTextMail( Email email )
+    {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setSubject( email.getSubject() );
+        message.setText( getText( email ) );
+        message.setFrom( email.getAuthor().getPrimaryEmail() );
+        message.setCc( email.getAuthor().getPrimaryEmail() );
+        String addresses[] = getAddresses( email.getRecipients(),
+            email.isUseSecondaryEmail() ).toArray( new String[0] );
+        if( addresses.length > 1 )
+        {
+            message.setTo( appEmail );
+            message.setBcc( addresses );
+        }
+        else
+            message.setTo( addresses );
+
+        mailSender.send( message );
+
+        logger.info( email.getAuthor().getUsername() + " sent email to "
+            + addresses.toString() );
+
+        return true;
+    }
+
+    public boolean sendHtmlMail( Email email )
+    {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper( message );
+
+        try
+        {
+            message.setContent( getHtml( email ), "text/html" );
+
+            helper.setSubject( email.getSubject() );
+            helper.setFrom( email.getAuthor().getPrimaryEmail() );
+            helper.setCc( email.getAuthor().getPrimaryEmail() );
+            String addresses[] = getAddresses( email.getRecipients(),
+                email.isUseSecondaryEmail() ).toArray( new String[0] );
+            if( addresses.length > 1 )
+            {
+                helper.setTo( appEmail );
+                helper.setBcc( addresses );
+            }
+            else
+                helper.setTo( addresses );
+
+            mailSender.send( message );
+
+            logger.info( email.getAuthor().getUsername() + " sent email to "
+                + addresses.toString() );
+
+            return true;
+        }
+        catch( MessagingException e )
+        {
+            logger.warn( "Fail to send MIME message", e );
+        }
+
+        return false;
     }
 
     public String getAppUrl()
